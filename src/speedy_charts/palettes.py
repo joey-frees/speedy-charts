925e1da67cf6162307e61b03422b8d99cc80f2a5
joefr <joe.freestone222@gmail.com>
2025-04-25 12:55:56 +0100

    Add additional palettes

--- src/speedy_charts/palettes.py
+++ src/speedy_charts/palettes.py
@@ -1,1 +1,17 @@
 af_categorical = ['#12436D', '#28A197', '#801650', '#F46A25', '#F46A25', '#3D3D3D', '#A285D1']
+
+std_diverging_5 = ["#003479", "#858AB3", "#EEEEEE",  "#F19695", "#DD2745"]
+std_diverging_7 = ["#003479", "#636C9F", "#A8AAC6", "#EEEEEE", "#F3B4B2", "#EC7779", "#DD2745"]
+std_diverging_9 = ["#003479", "#505D96", "#858AB3", "#B9BBD0", "#EEEEEE", "#F3B4B2", "#F19695", "#E9666C", "#DD2745"]
+
+oth_diverging_5 = []
+oth_diverging_7 = []
+oth_diverging_9 = ["#003479", "#505D96", "#858AB3", "#B9BBD0", "#EEEEEE", "#C2DDDB", "#95CCC9", "#62BBB7", "#00A9A5"]
+
+onehue_sequential_5 = []
+onehue_sequential_7 = []
+onehue_sequential_9 = ["#D4F7FF", "#B2DFEE", "#91C6DD", "#72ADCE", "#5595BF", "#3B7CAF", "#23649E", "#0D4C8C", "#003479"]
+
+oth_sequential_5 = []
+oth_sequential_7 = []
+oth_sequential_9 = ["#D9D9D9", "#C1C1C1", "#AAAAAA", "#929292", "#7C7C7C", "#646464", "#4B4B4B", "#323232", "#222222"]
